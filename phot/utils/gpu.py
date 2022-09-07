9b0675c355b4b866658f11efdc2eceec7b912948
lichunyu <cyli0212@gmail.com>
2022-09-07 19:49:27 +0800

    Add author comment in every source code file

--- phot/utils/gpu.py
+++ phot/utils/gpu.py
@@ -1,3 +1,20 @@
+"""
+Copyright (c) 2022 Beijing Jiaotong University
+PhotLab is licensed under [Open Source License].
+You can use this software according to the terms and conditions of the [Open Source License].
+You may obtain a copy of [Open Source License] at: [https://open.source.license/]
+
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+
+See the [Open Source License] for more details.
+
+Author: Chunyu Li
+Created: 2022/9/6
+Supported by: National Key Research and Development Program of China
+"""
+
 from .logging import logger
 import numpy as np
 
