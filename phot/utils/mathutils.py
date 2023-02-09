9cecd56ec2e52d507f45031025503103afac45bd
lichunyu <cyli0212@gmail.com>
2023-02-09 17:04:00 +0800

    Modified toml: "is_class" and "unit_type"

--- phot/utils/mathutils.py
+++ phot/utils/mathutils.py
@@ -18,17 +18,17 @@ Supported by: National Key Research and Development Program of China
 import numpy as np
 
 
-def gen_bits(length):
+def gen_bits(num_bits):
     """
     Generate random 0 1 bits sequence
 
     Args:
-        length: length of generated sequence
+        num_bits: how many bits we generated
         seed: generator seed
 
     Returns:
 
     """
-    data_x = np.random.randint(0, 2, (length, 1))  # 采用randint函数随机产生0 1码元序列x
-    data_y = np.random.randint(0, 2, (length, 1))  # 采用randint函数随机产生0 1码元序列x
+    data_x = np.random.randint(0, 2, (num_bits, 1))  # 采用randint函数随机产生0 1码元序列x
+    data_y = np.random.randint(0, 2, (num_bits, 1))  # 采用randint函数随机产生0 1码元序列x
     return data_x, data_y
